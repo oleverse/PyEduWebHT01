48adbb700a65b3705846b34de652c2479301dc87
Greezli439 <115574069+Greezli439@users.noreply.github.com>
2023-03-22 14:31:54 +0200

    test, add adress_book to menu

--- assistant/Main_menu.py
+++ assistant/Main_menu.py
@@ -1,5 +1,5 @@
 from arrange_dir import arrange_dir
-# from main_adrees_book import main as adress_book
+from main_address_book import main as adress_book
 from main_note import main as note_book
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
@@ -31,21 +31,26 @@ def goodby():
     exit(0)
 
 def start_arrange_dir():
-    dir = input('Give me the link to the dir: ')  #/Users/mykhailo/studies/go_it/my_little_assistant/arrange_dir/
+    try:
+        dir = input('Give me the link to the dir: ')  #/Users/mykhailo/studies/go_it/my_little_assistant/arrange_dir/
+    except:
+        start_arrange_dir()
     arrange_dir(dir)
 
 def start_note_book():
     note_book()
 
 def start_adress_book():
-    pass
-    # adress_book()
+    adress_book()
 
 COMMANDS = ['help', 'hello', 'hi', 'make jok', 'close', 'quit', 'by', 'goodby',
-            '1', 'arrange_dir', '2', 'note', 'add note', '3', 'phone_book', 'address_book', 'call']
-FUNCTIONS = [print_about_mla, print_hello, print_hello,
-             print_joke, goodby, goodby, goodby, goodby,
-             start_arrange_dir, start_arrange_dir, start_note_book, start_note_book]
+            '3', 'arrange_dir', '2', 'note', 'add note', '1', 'phone_book',
+            'address_book', 'call']
+FUNCTIONS = [print_about_mla, print_hello, print_hello, print_joke, goodby,
+             goodby, goodby, goodby, start_arrange_dir, start_arrange_dir,
+             start_note_book, start_note_book, start_adress_book,
+             start_adress_book, start_adress_book, start_adress_book,
+             start_adress_book]
 COMANDS_DICT = dict(zip(COMMANDS, FUNCTIONS))
 
 
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='My_little_assistant',
-      version='1.0.0',
+      version='1.0.1',
       description='Very useful code',
       url='https://github.com/Greezli439/my_little_assistant',
-      author='Command_name',
+      author='Python Mode',
       author_email='flyingcircus@example.com',
       license='MIT',
       packages = find_namespace_packages(),
       include_package_data = True,
-      entry_points = {'console_scripts': ['assistant = my_little_assistant:Main_menu']}
+      entry_points = {'console_scripts': ['assistant = assistant.Main_menu:main']}
       )
