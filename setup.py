73e3e782aa315c7f9c6654f543143949168f8e80
Greezli439 <115574069+Greezli439@users.noreply.github.com>
2023-03-22 16:37:54 +0200

    change name to THE_SOFT

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_namespace_packages
 
-setup(name='My_little_assistant',
-      version='1.0.2',
+setup(name='The_soft',
+      version='1.0.0',
       description='Sophisticated organizer for thoughtful',
       url='https://github.com/Greezli439/my_little_assistant',
       author='Python Mode',
-      author_email='flyingcircus@example.com',
       license='MIT',
       packages = find_namespace_packages(),
       include_package_data = True,
