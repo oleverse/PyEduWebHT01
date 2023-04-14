cdb83ef9a74b90289b9cde3cc79c69c4859fcb14
Oleksii Petrashov <oleksii.petrashov@gmail.com>
2023-04-14 23:22:31 +0300

    Parent dir added to sys.path; requirements.txt regenerated with poetry

--- the_soft/__main__.py
+++ the_soft/__main__.py
@@ -1,4 +1,11 @@
+from os.path import dirname, abspath, join
 import sys
+
+# add parent directory to sys.path
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '..'))
+sys.path.append(CODE_DIR)
+
 from the_soft import main
 
 if __name__ == '__main__':
